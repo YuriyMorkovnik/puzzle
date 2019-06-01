2296e9e0e1dbe3e85f9b2f335612bdac98679561
yuriymorkovnik <yuriymork@yandex.ru>
2019-06-01 16:55:58 +0300

    Updates

---
+++ precache-manifest.01cac10528e761c3096be17383cfab7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e68ad61d7580f41d9c4849ddcf0cfb8",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "16078cbcd353f5f0bdde",
+    "url": "/repository-name/static/css/main.5aaba3d6.chunk.css"
+  },
+  {
+    "revision": "b23b44a0de70359157ed",
+    "url": "/repository-name/static/js/2.4dd5dd58.chunk.js"
+  },
+  {
+    "revision": "16078cbcd353f5f0bdde",
+    "url": "/repository-name/static/js/main.238703ed.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  }
+]);
